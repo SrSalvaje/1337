35cad2eb0310629128064808f60e6b8a0a7e35e1
Jon Aresti Olin <jon.aresti@verisure.com>
2021-09-18 17:50:45 +0200

    remove CRApp fluff and add header

---
+++ src/components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = (props) => {
+    return (
+        <header>
+            <h1> The Fellowshipt of the tretton37</h1>
+        </header>
+    )
+}
+
+export default Header
