662ed4bf7604b85852b5f2d6a320cfadcbe2ca71
Jon Aresti Olin <jon.aresti@verisure.com>
2021-09-18 20:49:32 +0200

    use css modules for App.js and add basic styles

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import React, {useState, useEffect} from "react";
 import styles from './App.module.css';
 import Header from "./components/Header/Header";
 import SearchBar from "./components/SearchBar/SearchBar";
 import CardGrid from "./components/CardGrid/CardGrid";
-
+import mock from "./fakeData";
+import Card from "./components/Card/Card";
 
 function App() {
+
+
   return (
     <div className={styles.App}>
         <Header/>
         <SearchBar/>
         <CardGrid>
-
+            {mock.map(employee => <Card
+                image={employee.imagePortraitUrl}
+                name={employee.name}
+                socialMedia={employee.twitter}
+                office={employee.office}
+            />)}
         </CardGrid>
     </div>
   );
